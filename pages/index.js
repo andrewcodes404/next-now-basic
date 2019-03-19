cebe89ce888b1bcfb547c566d075fa620fdd6cc4
andrewcodes404 <andrewcodes404@gmail.com>
2019-03-19 15:29:12 +0000

    prima

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi 👋</h1>
+      </div>
+    )
+  }
+}
